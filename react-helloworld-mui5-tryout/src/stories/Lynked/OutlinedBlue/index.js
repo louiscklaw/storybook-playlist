88c314b3b95c91f43915d9813126cfa20aef4029
louiscklaw <louiscklaw@gmail.com>
2022-05-11 02:10:01 +0800

    update

--- react-helloworld-mui5-tryout/src/stories/Lynked/OutlinedBlue/index.js
+++ react-helloworld-mui5-tryout/src/stories/Lynked/OutlinedBlue/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import AlertNewServieRequest from '../../../components/Lynked/AlertNewServieRequest';
+import OutlinedBlue from '../../../components/OutlinedBlue';
 
 export const SBOutlineFood = ({
   primary,
@@ -10,7 +10,7 @@ export const SBOutlineFood = ({
   price,
   ...props
 }) => {
-  return <AlertNewServieRequest {...props} />;
+  return <OutlinedBlue {...props} />;
 };
 
 SBOutlineFood.propTypes = {
