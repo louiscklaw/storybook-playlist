31384d4b119bdf69bc063d03cf99d01c12fd64d5
louiscklaw <louiscklaw@gmail.com>
2022-05-10 18:31:58 +0800

    update

--- react-helloworld-mui5-tryout/src/stories/Lynked/HelloworldOutlineFood/index.stories.js
+++ react-helloworld-mui5-tryout/src/stories/Lynked/HelloworldOutlineFood/index.stories.js
@@ -16,11 +16,3 @@ Primary.args = {
   bar_olor: '#192a56',
   price: 9999,
 };
-
-export const Red = Template.bind({});
-Red.args = {
-  primary: true,
-  food_name: '阪ハウワ応92勤常勉旋72依',
-  bar_olor: '#800000',
-  price: 9999,
-};
