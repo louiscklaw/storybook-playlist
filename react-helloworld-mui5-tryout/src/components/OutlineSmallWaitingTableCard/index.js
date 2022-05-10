39aa2ccc876498976af5981d7dee4c949ff140f2
louiscklaw <louiscklaw@gmail.com>
2022-05-11 02:15:36 +0800

    update

--- react-helloworld-mui5-tryout/src/components/OutlineSmallWaitingTableCard/index.js
+++ react-helloworld-mui5-tryout/src/components/OutlineSmallWaitingTableCard/index.js
@@ -6,42 +6,44 @@ export default () => {
   return useMemo(
     () => (
       <>
-        <Grid container>
-          <Grid item xs={12} xl={4}></Grid>
-          <Grid
-            sx={{
-              borderRadius: '8px',
-              boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)',
-              backgroundColor: '#fff',
-            }}
-            container
-            justifyContent="space-between"
-            alignItems="center"
-            p={2}
-          >
+        <Box p={'0.5rem'} sx={{ maxWidth: '300px' }}>
+          <Grid container>
+            <Grid item xs={12} xl={4}></Grid>
             <Grid
-              item
-              xs={12}
+              sx={{
+                borderRadius: '8px',
+                boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)',
+                backgroundColor: '#fff',
+              }}
               container
-              justifyContent={'flex-start'}
+              justifyContent="space-between"
               alignItems="center"
+              p={2}
             >
-              <Grid item xs={8} sx={{ textAlign: 'left' }}>
-                <Typography variant="caption" sx={{ paddingLeft: '0.2rem' }}>
-                  2022年04月21日 19時28分
-                </Typography>
+              <Grid
+                item
+                xs={12}
+                container
+                justifyContent={'flex-start'}
+                alignItems="center"
+              >
+                <Grid item xs={8} sx={{ textAlign: 'left' }}>
+                  <Typography variant="caption" sx={{ paddingLeft: '0.2rem' }}>
+                    2022年04月21日 19時28分
+                  </Typography>
+                </Grid>
+                <Grid item xs={4} sx={{ textAlign: 'right' }}>
+                  <Typography variant="subtitle2">受付番号3</Typography>
+                </Grid>
               </Grid>
-              <Grid item xs={4} sx={{ textAlign: 'right' }}>
-                <Typography variant="subtitle2">受付番号3</Typography>
+              <Grid item xs={12} style={{ borderTop: '1px solid #ccc' }} mt={2}>
+                <Box mt={2} sx={{ textAlign: 'left' }}>
+                  <Typography variant="subtitle2">1 大人1 / 子供0</Typography>
+                </Box>
               </Grid>
-            </Grid>
-            <Grid item xs={12} style={{ borderTop: '1px solid #ccc' }} mt={2}>
-              <Box mt={2} sx={{ textAlign: 'left' }}>
-                <Typography variant="subtitle2">1 大人1 / 子供0</Typography>
-              </Box>
             </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </>
     ),
     []
